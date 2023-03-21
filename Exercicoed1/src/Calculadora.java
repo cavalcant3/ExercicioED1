e3c2e4c498417589610f9256e926220fa36962dc
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-03-21 20:02:48 -0300

    att

---
+++ Exercicoed1/src/Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora {
+    private int  resultado;
+
+
+
+    
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+}
--- Exercicoed1/src/Main.java
+++ Exercicoed1/src/Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Calculadora calculadora = new Calculadora();
         Scanner ler = new Scanner(System.in);
 
 
         System.out.println("Informe a operação que vocÊ deseja usar" +
                 "1 - adicao" +
                 "2 - multiplcacao");
-        int resposta;
-       resposta = ler.nextInt();
+        int resposta = calculadora.getResultado() ;
+        resposta = ler.nextInt();
         System.out.println(resposta);
     }
 
